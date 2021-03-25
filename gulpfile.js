026a51aa6751cea860a1a8a1cc848047f5422d69
Can Berk Sanal <sanalcanberk@gmail.com>
2021-03-25 21:54:15 +0100

    commit

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,12 @@
-var gulp = require("gulp");
-var sass = require("gulp-dart-sass");
+// var gulp = require("gulp");
+// var sass = require("gulp-dart-sass");
 
-gulp.task("css", function () {
-  return gulp
-    .src("./styles/main.scss")
-    .pipe(sass())
-    .pipe(gulp.dest("./styles/"));
-});
-gulp.task("default", function () {
-  gulp.watch("./styles/**/*.scss", gulp.series("css"));
-});
+// gulp.task("css", function () {
+//   return gulp
+//     .src("./styles/main.scss")
+//     .pipe(sass())
+//     .pipe(gulp.dest("./styles/"));
+// });
+// gulp.task("default", function () {
+//   gulp.watch("./styles/**/*.scss", gulp.series("css"));
+// });
